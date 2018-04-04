1774ac892061a9c70041e3d87d76107e7876be64
Dulguun Enkhsaikhan <mangode0808@github.com>
2018-04-04 16:09:58 -0500

    update Tweeter

--- liri.js
+++ liri.js
@@ -1,104 +1,242 @@
+// require("dotenv").config();
+// var keys = require('./keys.js');
+// var Twitter = require("twitter");
+// var Spotify = require("node-spotify-api");
+// var request = require("request");
+
+
+
+
+// var client = new Twitter(keys.twitter);
+// var spotify = new Spotify(keys.spotify);
+
+// var nodeArgv = process.argv;
+// var command = process.argv[2];
+
+// //movie or song
+// var input = "";
+
+// for (var i = 3; i < nodeArgv.length; i++) {
+//     if (i > 3 && i < nodeArgv.length) {
+//         input = input + "+" + nodeArgv[i];
+//     } else {
+//         input = input + nodeArgv[i];
+//     }
+// }
+
+
+// switch (command) {
+//     case "my-tweets":
+//         showTweets();
+//         break;
+
+//     case "spotify-this-song":
+//         if (input) {
+//             spotifySong(input);
+//         } else {
+//             spotifySong("Hot Bling");
+//         }
+//         break;
+
+//     case "movie-this":
+//         if(input) {
+//             omdbData(input)
+//         } else {
+//             omdbData("Avatar")
+//         }
+//         break;
+
+//     default:
+//         console.log("{Please enter a command: my-tweets, spotify-this-song, movie-this}");
+//         break;
+// }
+
+// //TWITTER
+// function showTweets() {
+// var params = { screen_name: 'JakeBoldbaatar' };
+// client.get('statuses/user_timeline', params, function (error, tweets, response) {
+//     if (!error) {
+//         for (var i = 0; i < tweets.length; i++) {
+//             console.log("\n===== Twitter text number: " + i + " =====");
+//             console.log("' " + tweets[i].text + " '");
+//             console.log("  Created at: " + tweets[i].created_at + "\n");
+
+//         }
+//     }
+// });
+// }
+
+// //SPOTIFY
+// function spotifySong(input) {
+//     spotify.search({ type: 'track', query: input, limit: 20 }, function (err, data) {
+//         if (err) {
+//             return console.log('Error occurred: ' + err);
+//         }
+//         for (var i = 0; i < data.tracks.items.length; i++) {
+//             console.log("\n==================\nArtist: " + data.tracks.items[i].artists[0].name);
+//             console.log("Song: " + data.tracks.items[i].name);
+//             console.log("Preview URL: " + data.tracks.items[i].preview_url);
+//             console.log("Album: " + data.tracks.items[i].album.name + "\n");
+//         }
+//     })
+// }
+
+// //OMDB
+
+// function omdbData (movie){
+//     var omdbURL = 'http://www.omdbapi.com/?t=' + movie + '&plot=short&apikey=trilogy';
+
+//     request(omdbURL, function(error, response, body){
+//         if(!error && response.statusCode == 200) {
+//             var body = JSON.parse(body);
+
+//             console.log("\nTitle: " + body.Title);
+//             console.log("Release Year: " + body.Year);
+//             console.log("IMdB Rating: " + body.imdbRating);
+//             console.log("Rotten Tomatoes Rating: " + body.tomatoRating);
+//             console.log("Country: " + body.Country);
+//             console.log("Language: " + body.Language);
+//             console.log("Plot: " + body.Plot);
+//             console.log("Actors: " + body.Actors + "\n");
+//         }
+//     })
+
+// }
 require("dotenv").config();
-var keys = require('./keys.js');
+var keys = require("./keys");
 var Twitter = require("twitter");
 var Spotify = require("node-spotify-api");
 var request = require("request");
-
-
-
-
-var client = new Twitter(keys.twitter);
+var fs = require("fs");
 var spotify = new Spotify(keys.spotify);
 
-var nodeArgv = process.argv;
-var command = process.argv[2];
 
-//movie or song
-var input = "";
-
-for (var i = 3; i < nodeArgv.length; i++) {
-    if (i > 3 && i < nodeArgv.length) {
-        input = input + "+" + nodeArgv[i];
-    } else {
-        input = input + nodeArgv[i];
+var writeToLog = function(data) {
+  fs.appendFile("log.txt", JSON.stringify(data) + "\n", function(err) {
+    if (err) {
+      return console.log(err);
     }
-}
 
+    console.log("log.txt was updated!");
+  });
+};
 
-switch (command) {
-    case "my-tweets":
-        showTweets();
-        break;
+var getArtistNames = function(artist) {
+  return artist.name;
+};
+var getMeSpotify = function(songName) {
+  if (songName === undefined) {
+    songName = "Hot Bling";
+  }
 
-    case "spotify-this-song":
-        if (input) {
-            spotifySong(input);
-        } else {
-            spotifySong("Hot Bling");
-        }
-        break;
+  spotify.search({ type: "track", query: songName }, function(err, data) {
+    if (err) {
+      console.log("Error occurred: " + err);
+      return;
+    }
 
-    case "movie-this":
-        if(input) {
-            omdbData(input)
-        } else {
-            omdbData("Avatar")
-        }
-        break;
+    var songs = data.tracks.items;
+    var data = [];
 
-    default:
-        console.log("{Please enter a command: my-tweets, spotify-this-song, movie-this}");
-        break;
-}
+    for (var i = 0; i < songs.length; i++) {
+      data.push({
+        "artist(s)": songs[i].artists.map(getArtistNames),
+        "song name: ": songs[i].name,
+        "preview song: ": songs[i].preview_url,
+        "album: ": songs[i].album.name
+      });
+    }
 
-//TWITTER
-function showTweets() {
-var params = { screen_name: 'JakeBoldbaatar' };
-client.get('statuses/user_timeline', params, function (error, tweets, response) {
+    console.log(data);
+    writeToLog(data);
+  });
+};
+
+var getMyTweets = function() {
+  var client = new Twitter(keys.twitter);
+
+  var params = { screen_name: "dulguun enkhsaikhan" };
+  client.get("statuses/user_timeline", params, function(error, tweets, response) {
     if (!error) {
-        for (var i = 0; i < tweets.length; i++) {
-            console.log("\n===== Twitter text number: " + i + " =====");
-            console.log("' " + tweets[i].text + " '");
-            console.log("  Created at: " + tweets[i].created_at + "\n");
+      var data = [];
 
-        }
+      for (var i = 0; i < tweets.length; i++) {
+        data.push({
+          created_at: tweets[i].created_at,
+          text: tweets[i].text
+        });
+      }
+
+      console.log(data);
+      writeToLog(data);
     }
-});
-}
+  });
+};
+var getMeMovie = function(movieName) {
+  if (movieName === undefined) {
+    movieName = "Avatar";
+  }
 
-//SPOTIFY
-function spotifySong(input) {
-    spotify.search({ type: 'track', query: input, limit: 20 }, function (err, data) {
-        if (err) {
-            return console.log('Error occurred: ' + err);
-        }
-        for (var i = 0; i < data.tracks.items.length; i++) {
-            console.log("\n==================\nArtist: " + data.tracks.items[i].artists[0].name);
-            console.log("Song: " + data.tracks.items[i].name);
-            console.log("Preview URL: " + data.tracks.items[i].preview_url);
-            console.log("Album: " + data.tracks.items[i].album.name + "\n");
-        }
-    })
-}
+  var urlHit = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=full&tomatoes=true&apikey=trilogy";
 
-//OMDB
+  request(urlHit, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      var jsonData = JSON.parse(body);
 
-function omdbData (movie){
-    var omdbURL = 'http://www.omdbapi.com/?t=' + movie + '&plot=short&apikey=trilogy';
+      var data = {
+        "Title:": jsonData.Title,
+        "Year:": jsonData.Year,
+        "Rated:": jsonData.Rated,
+        "IMDB Rating:": jsonData.imdbRating,
+        "Country:": jsonData.Country,
+        "Language:": jsonData.Language,
+        "Plot:": jsonData.Plot,
+        "Actors:": jsonData.Actors,
+        "Rotton Tomatoes Rating:": jsonData.Ratings[1].Value
+      };
 
-    request(omdbURL, function(error, response, body){
-        if(!error && response.statusCode == 200) {
-            var body = JSON.parse(body);
+      console.log(data);
+      writeToLog(data);
+    }
+  });
+};
 
-            console.log("\nTitle: " + body.Title);
-            console.log("Release Year: " + body.Year);
-            console.log("IMdB Rating: " + body.imdbRating);
-            console.log("Rotten Tomatoes Rating: " + body.tomatoRating);
-            console.log("Country: " + body.Country);
-            console.log("Language: " + body.Language);
-            console.log("Plot: " + body.Plot);
-            console.log("Actors: " + body.Actors + "\n");
-        }
-    })
+var doWhatItSays = function() {
+  fs.readFile("random.txt", "utf8", function(error, data) {
+    console.log(data);
 
-}
+    var dataArr = data.split(",");
+
+    if (dataArr.length === 2) {
+      pick(dataArr[0], dataArr[1]);
+    }
+    else if (dataArr.length === 1) {
+      pick(dataArr[0]);
+    }
+  });
+};
+
+var pick = function(caseData, functionData) {
+  switch (caseData) {
+  case "my-tweets":
+    getMyTweets();
+    break;
+  case "spotify-this-song":
+    getMeSpotify(functionData);
+    break;
+  case "movie-this":
+    getMeMovie(functionData);
+    break;
+  case "do-what-it-says":
+    doWhatItSays();
+    break;
+  default:
+    console.log("LIRI doesn't know that");
+  }
+};
+
+var runThis = function(argOne, argTwo) {
+  pick(argOne, argTwo);
+};
+
+runThis(process.argv[2], process.argv[3]);
